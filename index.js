0a97148610b967c244abfdef4cb91bf42080f5b0
Gandib Roy <gandibroy11@gmail.com>
2022-09-17 21:15:59 +0600

    payment updated

--- index.js
+++ index.js
@@ -62,6 +62,20 @@ async function run() {
             }
         }
 
+
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const service = req.body;
+            const price = service.totalPrice;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
+
+
         //get admin
         app.get('/admin/:email', async (req, res) => {
             const email = req.params.email;
