931341189ef8743889fb9af4b5af0ad54951e8c6
Gandib Roy <gandibroy11@gmail.com>
2022-09-18 13:48:30 +0600

    heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 4200;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
